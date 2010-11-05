2eeb8ca3422a3e021155a73b0eb6de4bcde44a41
David Padilla <david@crowdint.com>
2010-11-05 16:24:11 -0700

    Ready to begin

---
+++ lib/better_counter_caches.rb
@@ -0,0 +1,3 @@
+module BetterCounterCaches
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'test/unit'
+require 'bundler/setup'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
